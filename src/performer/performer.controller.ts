6cb5b76566b46452cedda667ade4f13cc325ccb1
Edd1e <estebanrco@hotmail.com>
2023-11-26 15:57:10 -0500

    performer controller done

--- src/performer/performer.controller.ts
+++ src/performer/performer.controller.ts
@@ -12,9 +12,31 @@ import {
 import { PerformerEntity } from './performer.entity/performer.entity';
 import { PerformerService } from './performer.service';
 import { BusinessErrorsInterceptor } from '../shared/interceptors/business-errors/business-errors.interceptor';
-
+import { PerformerDto } from './performer.dto/performer.dto';
+import { plainToInstance } from 'class-transformer';
 @Controller('performer')
 @UseInterceptors(BusinessErrorsInterceptor)
 export class PerformerController {
   constructor(private readonly performerservice: PerformerService) {}
+
+  @Put()
+  async createPerformer(
+    @Body() performer: PerformerDto,
+  ): Promise<PerformerEntity> {
+    const newPerformer: PerformerEntity = plainToInstance(
+      PerformerEntity,
+      performer,
+    );
+    return await this.performerservice.createPerformer(newPerformer);
+  }
+
+  @Get()
+  async findAll(): Promise<PerformerEntity[]> {
+    return await this.performerservice.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string): Promise<PerformerEntity> {
+    return await this.performerservice.findOne(id);
+  }
 }
